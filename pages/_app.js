cb31b897e06a482887fa45da77d9d307a03c09ad
night prince <night.prince.leo@gmail.com>
2026-05-12 02:58:06 -0400

    Add Vercel Analytics component to track user interactions and improve data insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import { ContactModalProvider } from '../components/ContactModal';
 import { ImageLightboxProvider } from '../components/ImageLightbox';
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }) {
     <ContactModalProvider>
       <ImageLightboxProvider>
         <Component {...pageProps} />
+        <Analytics />
       </ImageLightboxProvider>
     </ContactModalProvider>
   );
